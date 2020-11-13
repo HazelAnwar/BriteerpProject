0ad9d8e1b35516891db75b932663dc7b43306d4e
Nurshat01 <nuradam3033@gmail.com>
2020-11-13 09:03:09 -0600

    Runner class set up

---
+++ src/test/java/Runner/CucumberRunner.java
@@ -0,0 +1,20 @@
+package Runner;
+
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/resources/feature",
+        glue = "StepDefinition",
+        tags ="" ,
+        dryRun = true,
+        publish = true,plugin = "html:target/report.html"
+
+)
+
+public class CucumberRunner {
+
+}
