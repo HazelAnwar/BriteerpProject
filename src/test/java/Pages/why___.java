7634190a5771a75ac40fa1344f61c7452d49b7c6
Nurshat01 <nuradam3033@gmail.com>
2020-11-16 23:08:35 -0600

    why

--- src/test/java/Pages/why___.java
+++ src/test/java/Pages/why___.java
@@ -1,10 +1,10 @@
 package Pages;
 
-public class Test3 {
+public class why___ {
     public static void main(String[] args) {
 
         System.out.println("hello");
-
+        System.out.println("Hello");
         System.out.println("Hello");
 
     }
