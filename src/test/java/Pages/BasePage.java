c51da166e32141a4b1405462b96c5fa70beee3be
AsilRuz <a.ruzeaji@hotmail.com>
2020-11-17 15:00:17 +1030

    LoginPage

--- src/test/java/Pages/BasePage.java
+++ src/test/java/Pages/BasePage.java
@@ -15,25 +15,6 @@ public class BasePage {
         PageFactory.initElements(Driver.getDriver(),this);
     }
 
-    WebDriverWait wait=new WebDriverWait(Driver.getDriver(),10);
-    @FindBy(xpath = "//input[@id='login']")
-    private WebElement userNameInput;
-
-    @FindBy(id = "//input[@id='password']")
-    private WebElement passwordInput;
-
-    public void login(){
-        Driver.getDriver().get(ConfigurationReader.getProperties("BriteerpURL"));
-        userNameInput.sendKeys(ConfigurationReader.getProperties("BriteerpUsername"));
-        passwordInput.sendKeys(ConfigurationReader.getProperties("BriteerpPassword")+ Keys.ENTER);
-        WebElement homePage=Driver.getDriver().findElement(By.xpath("//li[contains(text(),'#Inbox')]"));
-        if (homePage.isDisplayed()){
-            System.out.println("You are on the Home Page. **Login Success** ");
-        }else {
-            System.out.println("You are not in the homepage **Login Failed**");
-            throw new RuntimeException("**Login Failed**");
-        }
-    }
 
     /**
      *
